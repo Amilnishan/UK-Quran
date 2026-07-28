a4af5f606694786b035e01c3af0475312719b63f
Adnan al uthan pp <adnan.al.uthan,pp7@gmail.com>
2026-07-28 14:38:47 +0530

    done

--- Components/Admin/admin.js
+++ Components/Admin/admin.js
@@ -472,5 +472,14 @@ btnConfirmDelete?.addEventListener('click', async () => {
     }
 });
 
-btnGotoTeacherView?.addEventListener('click', () => window.location.href = '../Teacher/teacher.html');
+btnGotoTeacherView?.addEventListener('click', () => {
+    if (!btnGotoTeacherView) return;
+
+    btnGotoTeacherView.disabled = true;
+    btnGotoTeacherView.innerHTML = '<span class="loader-inline1" aria-hidden="true"></span>Teacher View';
+
+    setTimeout(() => {
+        window.location.href = '../Teacher/teacher.html';
+    }, 120);
+});
 btnAdminLogout?.addEventListener('click', () => signOut(auth).then(() => window.location.href = '../../index.html'));
